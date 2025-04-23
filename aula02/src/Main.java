a03e023411cd05ea221677408530ee4ba839c3c4
Brunoaichinger <brunoaichinger@gmail.com>
2025-04-23 15:33:20 -0300

    Terceiro commit - exercicio cadastro formatado

--- aula02/src/Main.java
+++ aula02/src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Primeiro commit, linha 5");
-        System.out.printf("Segundo commit, linha 6\n");
+        System.out.println("Digite o seu nome:");
+        Scanner txt = new Scanner(System.in);
+        String nome = txt.nextLine();
+        System.out.println("Digite a sua idade:");
+        int idade = txt.nextInt();
+        System.out.println("Digite a sua altura:");
+        double altura = txt.nextDouble();
+        System.out.printf("Nome: %s\nAltura:%.2f\nIdade:%d", nome, altura, idade);
 
     }
 }
