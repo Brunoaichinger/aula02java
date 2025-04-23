bd233d921c6d29e7e7b9559c8a7d9f591369793b
Brunoaichinger <brunoaichinger@gmail.com>
2025-04-23 14:58:02 -0300

    Segundo commit, linha 06

--- aula02/src/Main.java
+++ aula02/src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Primeiro commit, linha 5");
+        System.out.printf("Segundo commit, linha 6\n");
 
     }
 }
